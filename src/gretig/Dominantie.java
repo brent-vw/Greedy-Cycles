3f454a447909f0992384ba335275990b7a4c5eec
brent <brent.van.wynsberge@gmail.com>
2016-10-07 10:45:40 +0200

    Initial commit

---
+++ src/gretig/Dominantie.java
@@ -0,0 +1,7 @@
+package gretig;
+
+/**
+ * Created by brent on 10/7/16.
+ */
+public class Dominantie {
+}
---
+++ src/gretig/Hamilton.java
@@ -0,0 +1,8 @@
+package gretig;
+
+/**
+ * Created by brent on 10/7/16.
+ */
+public class Hamilton {
+
+}
